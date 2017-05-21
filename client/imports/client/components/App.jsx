98295bcaaacb494630b720aa58e2c35e0d3cbe7c
Adam Grezak <adamgrezak@gmail.com>
2017-05-21 13:08:16 +0200

    adding mongo, basic styling

--- client/imports/client/components/App.jsx
+++ client/imports/client/components/App.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Whiteboard from './Whiteboard.jsx';
 
-export default class App extends Component {
+export default class App extends React.Component {
 
     render() {
         return (
             <div className="container">
                 <h1>Whiteboard App</h1>
-                <Whiteboard/>
+                <Whiteboard />
             </div>
         );
     }
--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import App from './imports/components/App.jsx';
+import App from './imports/client/components/App.jsx';
 
 Meteor.startup(() => {
     render(<App />, document.getElementById('app'));
